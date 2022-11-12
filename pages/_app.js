30dc14b8e69238402aa59866bbf6c30302ac02ed
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2022-11-12 12:09:16 -0400

    Add context to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-
+import {AppContextProvider} from '../src/Context/Ctx'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppContextProvider>
+      <Component {...pageProps} />
+    </AppContextProvider>
+    
+  )
 }
 
 export default MyApp
